7bfaafcdcc52d882109ba944d74788e27a760d5d
Findsay <fiona.e.lindsay@gmail.com>
2017-12-01 16:58:22 +0000

    Should have an artist test passed

--- specs/record_specs.js
+++ specs/record_specs.js
@@ -9,7 +9,9 @@ describe("Record", function(){
     record = new Record("Brand New", "Science Fiction", "Rock", 9.99);
   });
 
-  it("should have an artist");
+  it("should have an artist", function(){
+    assert.strictEqual(record.artist, "Brand New");
+  });
   it("should have a title");
   it("should have a genre");
   it("should have a price");
