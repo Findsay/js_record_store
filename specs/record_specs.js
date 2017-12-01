f76240cdfdc4823942c3e80c25b7285aab78f483
Findsay <fiona.e.lindsay@gmail.com>
2017-12-01 16:52:43 +0000

    Added pending tests for record

---
+++ specs/record_specs.js
@@ -0,0 +1,20 @@
+var assert = require("assert");
+var Record = require("../Record");
+
+describe("Record", function(){
+
+  var record;
+
+  beforeEach(function(){
+    record = new Record("Brand New", "Science Fiction", "Rock", 9.99);
+  });
+
+  it("should have an artist");
+  it("should have a title");
+  it("should have a genre");
+  it("should have a price");
+
+
+
+
+});
