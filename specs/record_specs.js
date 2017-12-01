f8bf257b46c1a49008b9098fc92130cf97c33252
Findsay <fiona.e.lindsay@gmail.com>
2017-12-01 17:10:26 +0000

    all record pending tests passing

--- specs/record_specs.js
+++ specs/record_specs.js
@@ -12,9 +12,15 @@ describe("Record", function(){
   it("should have an artist", function(){
     assert.strictEqual(record.artist, "Brand New");
   });
-  it("should have a title");
-  it("should have a genre");
-  it("should have a price");
+  it("should have a title", function(){
+    assert.strictEqual(record.title, "Science Fiction");
+  });
+  it("should have a genre", function(){
+    assert.strictEqual(record.genre, "Rock");
+  });
+  it("should have a price", function(){
+    assert.strictEqual(record.price, 9.99);
+  });
 
 
 
