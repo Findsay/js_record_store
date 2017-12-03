5c7c62643d84796267f579db6a233cb58c0cab28
Findsay <fiona.e.lindsay@gmail.com>
2017-12-03 13:46:25 +0000

    Set up pending tests for record store

---
+++ specs/record_store_spec.js
@@ -0,0 +1,19 @@
+var assert = require("assert");
+var RecordStore = require("../record_store");
+var Record = require("../record");
+
+describe("Record Store", function(){
+  var recordStore;
+  var record;
+
+  beforeEach(function(){
+    recordStore = new RecordStore("Flopp", "Glasgow");
+    record = new Record("Brand New", "Science Fiction", "Rock", 9.99);
+  });
+
+  it("should have a name");
+  it("should have a city");
+  it("should have an inventory, that starts empty");
+  it("should have a balance that starts at 0");
+  it('should be able to add records to the inventory');
+})
