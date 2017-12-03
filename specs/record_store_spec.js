3560a1177fb12fb78e65581cf373cbf213b31de5
Findsay <fiona.e.lindsay@gmail.com>
2017-12-03 13:49:30 +0000

    Record Store, should have city test passing

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -14,7 +14,9 @@ describe("Record Store", function(){
   it("should have a name", function(){
     assert.strictEqual(recordStore.name, "Flopp");
   });
-  it("should have a city");
+  it("should have a city", function(){
+    assert.strictEqual(recordStore.city, "Glasgow");
+  });
   it("should have an inventory, that starts empty");
   it("should have a balance that starts at 0");
   it('should be able to add records to the inventory');
